e2d62ba139127c44aa2f17ca63dc10e7ddddf4a9
vikasrajputvikas <rajputvikas9719@gmail.com>
2024-03-13 22:37:21 +0530

    parameter added

--- jenkinsdemo/src/test/java/jenkinsdemo/UITest.java
+++ jenkinsdemo/src/test/java/jenkinsdemo/UITest.java
@@ -2,25 +2,31 @@ package jenkinsdemo;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.Parameters;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class UITest {
-	
-
+	@Parameters("Browser")
 	@Test
-	public void startbrowser() throws InterruptedException {
+	public void startbrowser(String browsername) throws InterruptedException {
 		
-		WebDriverManager.chromedriver().setup();
-		WebDriver driver=new FirefoxDriver();
+		System.out.println("parameter vale :"+browsername);
+		WebDriver driver = null;
+		if(browsername.contains("Chrome")) {
+			WebDriverManager.chromedriver().setup();
+			driver=new ChromeDriver();
+		}else if(browsername.contains("Firefox")) {
+			WebDriverManager.firefoxdriver().setup();
+			driver=new FirefoxDriver();
+		}
+
 		driver.manage().window().maximize();
 		driver.get("https://google.com");
 		Thread.sleep(4000);
 		driver.quit();
-		
-		
-	}
-
+}
 }
